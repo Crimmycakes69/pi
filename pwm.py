0eeeca09c83a3eef32ab72d7c3188b1d0b502304
Crimsonfury9 <crimsonfury9@gmail.com>
2023-04-05 14:19:16 +0200

    pin reset to 18

--- pwm.py
+++ pwm.py
@@ -4,7 +4,7 @@ import time
 # Set up GPIO pins
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)  # Pin 12 for PWM1
-GPIO.setup(13, GPIO.OUT)  # Pin 32 for PWM2
+GPIO.setup(18, GPIO.OUT)  # Pin 32 for PWM2
 
 # Set up PWM frequency and duty cycle
 freq = 1000  # 1kHz frequency
@@ -13,7 +13,7 @@ duty_cycle_max = 100  # 100% duty cycle
 
 # Create PWM instances
 pwm1 = GPIO.PWM(12, freq)
-pwm2 = GPIO.PWM(13, freq)
+pwm2 = GPIO.PWM(18, freq)
 
 # Start PWM signals
 pwm1.start(duty_cycle_min)
