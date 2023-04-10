70a3e65781cee73e017daf6691c1ea778e813ddd
Crimsonfury9 <crimsonfury9@gmail.com>
2023-04-10 23:44:15 +0200

    bro idk

--- pwm.py
+++ pwm.py
@@ -1,24 +1,37 @@
+import RPi.GPIO as GPIO
 import time
-import rpi.gpio as rpgio
 
-G1=12
-G2=13
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
 
-FREQ=1000
-DUTY=50
+# Set the pin numbers for the PWM signals
+pwm_pin1 = 18
+pwm_pin2 = 19
 
-ON_MICS = int(1e6 / FREQ * DUTY / 100.0)
+# Set the frequency for the PWM signals
+frequency = 1000
 
-sbc = rgpio.sbc()
-if not sbc.connected:
-   exit()
+# Set up the PWM signals
+GPIO.setup(pwm_pin1, GPIO.OUT)
+GPIO.setup(pwm_pin2, GPIO.OUT)
+pwm1 = GPIO.PWM(pwm_pin1, frequency)
+pwm2 = GPIO.PWM(pwm_pin2, frequency)
 
-h = sbc.gpiochip_open(0) # Pi's main gpiochip
+# Start the PWM signals with duty cycle 0%
+pwm1.start(0)
+pwm2.start(0)
 
-sbc.gpio_claim_output(h, G1) # claim G1 of gpiochip
-sbc.gpio_claim_output(h, G2) # claim G2 of gpiochip
+# Loop to alternate between the two PWM signals
+while True:
+    # Set duty cycle to 100% for PWM signal 1 and 0% for PWM signal 2
+    pwm1.ChangeDutyCycle(100)
+    pwm2.ChangeDutyCycle(0)
+    time.sleep(1)  # Wait for 1 second
 
-sbc.tx_pwm(h, G1, FREQ, DUTY)
-sbc.tx_pwm(h, G2, FREQ, DUTY, pulse_offset=ON_MICS)
+    # Set duty cycle to 0% for PWM signal 1 and 100% for PWM signal 2
+    pwm1.ChangeDutyCycle(0)
+    pwm2.ChangeDutyCycle(100)
+    time.sleep(1)  # Wait for 1 second
 
-time.sleep(30)
+# Clean up the GPIO pins when finished
+GPIO.cleanup()
